227ff4e8d256ef6bfc2c654d46d8fb15a31ab279
Nitin <nitin.kishore04@gmail.com>
2022-08-03 13:54:35 +0530

    create scheduler

--- src/main/java/com/server/bayztracker/BayztrackerApplication.java
+++ src/main/java/com/server/bayztracker/BayztrackerApplication.java
@@ -2,8 +2,10 @@ package com.server.bayztracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BayztrackerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/server/bayztracker/cron/AlertScheduler.java
+++ src/main/java/com/server/bayztracker/cron/AlertScheduler.java
@@ -1,4 +1,35 @@
 package com.server.bayztracker.cron;
 
+import com.server.bayztracker.dao.AlertRepository;
+import com.server.bayztracker.dao.CurrencyRepository;
+import com.server.bayztracker.entity.Alert;
+import com.server.bayztracker.entity.Currency;
+import com.server.bayztracker.entity.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
 public class AlertScheduler {
+
+    @Autowired
+    private AlertRepository alertRepository;
+
+    @Autowired
+    private CurrencyRepository currencyRepository;
+
+    @Scheduled(cron = "0 0/30 * 1/1 * ? *")
+    public void sendAlert() {
+
+        Collection<Alert> alerts = alertRepository.findAllByStatus(Status.NEW);
+        alerts.forEach(alert -> {
+            Currency currency = currencyRepository.findBySymbolAndActive(alert.getCurrency(), Boolean.TRUE).get();
+            if (currency.getCurrentPrice() == alert.getTargetPrice()) {
+                System.out.println("Target Price Reached");
+            }
+        });
+
+    }
 }
